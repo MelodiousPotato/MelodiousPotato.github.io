0ed1d4cf9c1ffe17a5920cbd8ec39964ab396280
MelodiousPotato <87632596+MelodiousPotato@users.noreply.github.com>
2026-02-16 23:09:35 +0000

    Deploying to gh-pages from @ MelodiousPotato/Rebekah_Mou_Portfolio@a80bb4ebbc29642c43cdf23910761cd81d827363 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://MelodiousPotato.github.io/#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://melodiouspotato.github.io/#contact">contact us</a>.'
               }
             ]
           }
