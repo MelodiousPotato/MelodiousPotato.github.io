787720d3f2cc9a7116edae74ce827023edef201b
Melody <87632596+MelodiousPotato@users.noreply.github.com>
2026-03-28 13:20:56 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  basePath: '/MelodiousPotato.github.io',
+  assetPrefix: '/MelodiousPotato.github.io/',
 }
 
 module.exports = nextConfig
